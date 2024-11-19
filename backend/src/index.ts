6be74592a13a2f99d305903b9d72db70abf43ac1
Murasamy <pw2179@nyu.edu>
2024-11-19 16:19:58 -0600

    add session

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,12 +3,19 @@ import express from "express";
 import { Request, Response } from "express";
 import cors from "cors";
 import topRecipe from "../src/routes/topRecipe";
+import session from "express-session";
 
 const app = express();
 const PORT = 3007;
 
 app.use(cors());
 app.use(express.json());
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+}))
 
 app.get("/", (req, res) => {
     res.send("server is running");
