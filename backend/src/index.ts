5dece03602e1b3cd247a1802eb0118dcd3090d41
hyang14 <lyang01354@gmail.com>
2024-11-19 13:48:11 -0600

    database connected

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,8 @@
 // index.ts
-import express from 'express';
-import { Request, Response } from 'express';
-import cors from 'cors';
-import topRecipe from '../src/routes/topRecipe'
+import express from "express";
+import { Request, Response } from "express";
+import cors from "cors";
+import topRecipe from "../src/routes/topRecipe";
 
 const app = express();
 const PORT = 3007;
@@ -10,14 +10,16 @@ const PORT = 3007;
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/', (req: Request, res: Response) => {
-    res.send('Homepage of LettuceEat.');
+app.get("/", (req, res) => {
+    res.send("server is running");
+});
+
+app.get("/api/", (req: Request, res: Response) => {
+    res.send("Homepage of LettuceEat.");
 });
 
 app.use("/api/recipe", topRecipe);
 
-
 app.listen(PORT, () => {
     console.log(`LettuceEat is running on http://localhost:${PORT}`);
 });
-
